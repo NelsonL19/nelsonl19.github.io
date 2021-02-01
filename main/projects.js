2d87223d84b716ffa690dfe47bbf5e865e9da148
Nelson Lopez <43620036+NelsonL19@users.noreply.github.com>
2021-02-01 17:01:35 -0500

    Yes
    yes

---
+++ main/projects.js
@@ -0,0 +1,28 @@
+
+
+`<section class="hero is-link is-fullheight is-medium is-bold">
+<div class="hero-body">
+    <div class="container">
+        <div class="notification is-warning">
+            New Github Repos and Projects are being completed all the time, so check back often to see the
+            latest!
+        </div>
+
+        <div class="box">
+            <h1 class="title has-text-black">Comp 426 Projects</h1>
+            <h1 class="subtitle has-text-grey">Web Development</h1>
+            <button class="button is-link">2048</button>
+            <button class="button is-info">KMP's Spicy 9 Adventure</button>
+        </div>
+    </div>
+
+
+
+
+
+    <nav class="navbar is-spaced is-fixed-top" role="navigation" aria-label="dropdown navigation">
+        <a class="navbar-item" href="./index.html">
+            Back
+        </a>
+    </nav>
+</section>`
